69c8a574fe0e07f8482865b32526988c031ebbbd
Sumit1311 <sumittoshniwal92@gmail.com>
2021-02-02 13:13:26 +0530

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+struct Word {
+    std::string word;
+    int lineno;
+};
+
+class InputReader {
+    int lineno;
+
+    std::vector<Word> getWords() {
+
+    }
+};
+
+class WordInserter {
+    std::unordered_map<std::string, std::vector<int>> word_map;
+
+    void insert(Word word) {
+
+    }
+
+    begin()
+    end()
+};
+
+class IndexPrinter {
+    void print() {
+
+    }
+};
